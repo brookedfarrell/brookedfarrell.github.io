431404cb98e585e8a23101434379f8e9ee8e0c65
brookedfarrell <brookedfarrell@byui.edu>
2020-02-16 17:25:18 -0800

    cnz

--- lesson6/js/scripts.js
+++ lesson6/js/scripts.js
@@ -15,21 +15,3 @@ function showBanner(){
 }
 showBanner(); 
 
-function windChill(){
-const temp= parseFloat(document.getElementById("temp").textContent);
-const windSpeed= parseFloat(document.getElementById("windSpeed").textContent);
-
-let windchill= 35.74 + (0.6215 * temp)-(35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16));
-
-windchill= Math.round(windchill);
-
-if (temp<51 && windspeed >3) {
-    document.getElementById("chill").textContent= "Wind Chill Temperature: "+windchill + "F";
-}
-else
-{document.getElementById("chill").textContent = "Wind Chill Temperature: N/A";
-
-}
-
-}
-windChill();
--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,18 +1,17 @@
-function windChill(){
+
     const temp= parseFloat(document.getElementById("temp").textContent);
     const windSpeed= parseFloat(document.getElementById("windSpeed").textContent);
     
-    let windchill= 35.74 + (0.6215 * temp)-(35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16));
+    let windchill= 35.74 + (0.6215 * temp)-(35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
     
     windchill= Math.round(windchill);
     
-    if (temp<51 && windspeed >3) {
-        document.getElementById("chill").textContent= "Wind Chill Temperature: "+windchill + "F";
+    if (temp<51 && windSpeed >3) {
+        document.getElementById("windchill").innerHTML= "Wind Chill Temperature: "+windchill + "F";
     }
     else
-    {document.getElementById("chill").textContent = "Wind Chill Temperature: N/A";
+    {document.getElementById("windchill").innerHTML = "Wind Chill Temperature: N/A";
     
     }
     
-    }
-    windChill();
+  
