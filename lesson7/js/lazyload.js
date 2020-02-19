3fd7bf6babc39da12cff81b775b4e5b3ed76f168
brookedfarrell <brookedfarrell@byui.edu>
2020-02-18 23:05:00 -0800

    sdgas

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,33 @@
+// get all imgs with data-src attribute
+const imagesToLoad= document.querySelectorAll("img[data-src]");
+
+// optional parameters being set for the IntersectionalObserver
+const imgOptions= {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages= (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload= () => {image.removeAttribute("data-src");};
+};
+
+//first check to see if Intersection Observer is supported
+if("IntersectionObserver" in window) {
+    const imgObserver= new IntersectionObserver((items, observer) => {
+        items.forEach((item)=> {
+    });
+}, imgOptions);
+
+
+
+// loop through each img on check status and load if necessary
+imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+});
+}
+else { //just load ALL images if not supported
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
