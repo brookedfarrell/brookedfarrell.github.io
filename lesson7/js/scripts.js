0619ebbd0c7e6b73edcee97645103db82ff7eb34
brookedfarrell <brookedfarrell@byui.edu>
2020-02-22 21:36:40 -0800

    dkfgja;d

---
+++ lesson7/js/scripts.js
@@ -0,0 +1,17 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+function getDate(){
+    var x= new Date(document.lastModified);
+    document.getElementById("date").innerHTML= x;
+}
+function showBanner(){ 
+    var d = new Date();
+    if (d.getDay()==5){
+    
+    document.getElementById("banner").innerHTML= "Preston Pancakes in the Park! 9:00 am Saturday at the city park pavilion.";
+    document.getElementById("banner").style.display= "block";
+  }
+}
+showBanner(); 
+
