e3d6d91a20d8bafd63bcd0daa8a252efb284420a
brookedfarrell <brookedfarrell@byui.edu>
2020-03-06 22:03:31 -0800

    cvnzn

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -1,34 +1,40 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
 fetch(requestURL)
     .then(function(response){
-        //console.log(response);
         return response.json();
     })
-    .then(function(jsonObject){
+    .then(function (jsonObject){
         console.table(jsonObject);
-        const towns = jsonObject['towns'];
+        const towns= jsonObject['towns'];
+
         towns.forEach(town => {
-            let card = document.createElement('section');
+           
+           
+            let card= document.createElement('section');
             let h2 = document.createElement('h2');
-            //let motto = document.createElement('motto');
-            //let year = document.createElement('year');
-           // let pop = document.createElement('pop');
-            //let fall = document.createElement('fall');
+            let motto= document.createElement('motto');
+            let year= document.createElement('year');
+            let pop= document.createElement('pop');
+            let fall= document.createElement('fall');
+            let img= document.createElement('img');
 
+            
 
-            h2.textContent= `${name}`;
-            //motto.textContent= `${motto}`;
-            //year.textContent= `${yearFounded}`;
-            //pop.textContent= `${currentPopulation}`;
-            //fall.textContent= `${averageRainfall}`;
-
+            h2.textContent= `${town.name}`;
+            motto.textContent= `"${town.motto}"`;
+            year.textContent= `Year Founded: ${town.yearFounded}`;
+            pop.textContent= `Population: ${town.currentPopulation}`;
+            fall.textContent= `Annual Rain Fall: ${town.averageRainfall}`;
+          
+          
             card.appendChild(h2);
-            //card.appendChild(motto);
-            //card.appendChild(year);
-            //card.appendChild(pop);
-            //card.appendChild(fall);
-
+            card.appendChild(motto);
+            card.appendChild(year);
+            card.appendChild(pop);
+            card.appendChild(fall);
+            
             document.querySelector('.cards').appendChild(card);
-        });
+            
+        })
+
     });
--- lesson9/js/scripts.js
+++ lesson9/js/scripts.js
@@ -5,13 +5,13 @@ function getDate(){
     var x= new Date(document.lastModified);
     document.getElementById("date").innerHTML= x;
 }
-function showBanner(){ 
-    var d = new Date();
-    if (d.getDay()==5){
+//function showBanner(){ 
+   // var d = new Date();
+    //if (d.getDay()==5){
     
-    document.getElementById("banner").innerHTML= "Preston Pancakes in the Park! 9:00 am Saturday at the city park pavilion.";
-    document.getElementById("banner").style.display= "block";
-  }
-}
-showBanner(); 
+   // document.getElementById("banner").innerHTML= "Preston Pancakes in the Park! 9:00 am Saturday at the city park pavilion.";
+   // document.getElementById("banner").style.display= "block";
+  //}
+//}
+//showBanner(); 
 
