9e263ea19ad63c5da74934f478b5e05c26d51254
brookedfarrell <brookedfarrell@byui.edu>
2020-03-15 20:56:47 -0700

    fgafas

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -6,24 +6,25 @@ fetch (apiURL)
 
     const main = jsObject['main'];
     const wind = jsObject['wind'];
+    const weather = jsObject['weather'];
 
     let summary= document.createElement('section');
-    let h2 = document.createElement('h2');
+    let h2 = document.createElement('h3');
 
-    //let currently = document.createElement('p');
+    let currently = document.createElement('p');
     let temp = document.createElement('p');
     let humidity= document.createElement('p');
     let windSpeed = document.createElement('p');
     
 
     h2.textContent= `Weather Summary`;
-    //currently.textContent= `${weather.main}`;
-    temp.textContent= `Temp: ${main.temp}`;
+    currently.textContent= `Currently: ${weather.main}`;
+    temp.textContent= `Temp: ${main.temp}° F`;
     humidity.textContent = `Humidity: ${main.humidity}%`;
     windSpeed.textContent = `Wind Speed: ${wind.speed} MPH`;
 
     summary.appendChild(h2);
-    //summary.appendChild(currently);
+    summary.appendChild(currently);
     summary.appendChild(temp);
     summary.appendChild(humidity);
     summary.appendChild(windSpeed);
@@ -43,16 +44,16 @@ fetch (apiURL)
     const weekday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
     const fivedayforecast = jsObject.list.filter (x => x.dt_txt.includes ('18:00:00'));
         console.log(fivedayforecast);
-
+    
         let forecastUl = document.getElementById('forecastUl');
         let content = "";
         for (const item of fivedayforecast) {
             const dow = new Date(item.dt_txt);
 
-            content += `<li>
-                        ${item.main.temp}
+            content += `<li class= "dow">
+                        ${weekday[dow.getDay()]}                        
                         <img class= "wicon" src="https://openweathermap.org/img/w/${item.weather[0].icon}.png" alt ="">
-                        ${weekday[dow.getDay()]}
+                        ${item.main.temp}°F
                         </li>`;
             
         }
